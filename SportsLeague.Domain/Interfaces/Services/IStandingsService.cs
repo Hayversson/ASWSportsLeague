3609bc4b983345ad571e8bd21d9b1726030dbc79
Hayversson Jhair Mosquera Moreno <hayversson10one@gmail.com>
2026-05-16 11:29:18 -0500

    feat: standings and Stats

---
+++ SportsLeague.Domain/Interfaces/Services/IStandingsService.cs
@@ -0,0 +1,14 @@
+﻿namespace SportsLeague.Domain.Interfaces.Services;
+
+public interface IStandingsService
+{
+    // keyword o palabra reservada "object"
+    // se utiliza para indicar que el método puede devolver
+    // cualquier tipo de dato. En este caso, se espera que el
+    // método devuelva un objeto que contenga la información de
+    // la tabla de posiciones, los maximos goleadores o las estadisticas de tarjetas,
+    // dependiendo del método especifico.
+    Task<object> GetStandingsAsync(int tournamentId); // Obtener tabla de posiciones para un torneo específico
+    Task<object> GetTopScorersAsync(int tournamentId); // Obtener lista de goleadores para un torneo específico
+    Task<object> GetCardStatsAsync(int tournamentId); // Obtener estadísticas de tarjetas para un torneo específico
+}
